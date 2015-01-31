9a24fb70fded8d6eb5b25f933e22757b93bf4ee4
Jan Helbich <janhelbich@gmail.com>
2015-01-31 01:42:43 +0100

    Oprava chyb v projektu

--- Gourmeter/src/main/java/cz/cvut/fel/jee/gourmeter/dto/CategoryDTO.java
+++ Gourmeter/src/main/java/cz/cvut/fel/jee/gourmeter/dto/CategoryDTO.java
@@ -4,31 +4,31 @@ import cz.cvut.fel.jee.gourmeter.bo.Category;
 
 public class CategoryDTO {
 
-	private final Long id;
-  private final String name;
+	private Long id;
+	private String name;
 
 	public CategoryDTO() {
 	}
 
 	public CategoryDTO(Category c) {
-    id = c.getId();
+		id = c.getId();
 		name = c.getName();
 	}
 
-  public Long getId() {
+	public Long getId() {
 		return id;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
 	}
-  
+
 	public String getName() {
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	} 
+	}
 
 }
